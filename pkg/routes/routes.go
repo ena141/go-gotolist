83e3c44f0b739a3c89644ca7da9eaf4c1092f493
ena141 <takina141@gmail.com>
2024-06-04 12:56:55 +0900

    Add routes

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -1,1 +1,19 @@
 package routes
+
+import (
+	"github.com/ena141/go-gotolist/pkg/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitRoutes() *gin.Engine {
+	router := gin.Default()
+	router.LoadHTMLGlob("statics/*")
+
+	router.GET("/", controllers.GetTasks)
+	router.POST("/add", controllers.AddTask)
+	router.GET("/complete/:id", controllers.CompleteTask)
+	router.GET("/delete/:id", controllers.DeleteTask)
+
+	return router
+}
